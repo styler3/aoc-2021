a3d0217df666a70ff9adcdbab4d0a6a18d26ae10
Sam Tyler <sam@sammtyler.com>
2021-12-03 14:57:23 +1030

    Solve Day 2 part 2

--- day2/solution.js
+++ day2/solution.js
@@ -25,4 +25,34 @@ const finalPositionOne = input.reduce(({ horizontal, depth }, [direction, size])
   depth: 0
 })
 
+const finalPositionTwo = input.reduce(({ horizontal, depth, aim }, [direction, size]) => {
+  if (direction === 'down') {
+    return {
+      horizontal,
+      depth,
+      aim: aim + size
+    };
+  }
+  if (direction === 'up') {
+    return {
+      horizontal,
+      depth,
+      aim: aim - size
+    };
+  }
+  if (direction === 'forward') {
+    return {
+      horizontal: horizontal + size,
+      depth: depth + (aim * size),
+      aim
+    };
+  }
+  throw new Error('You fucked something up');
+}, {
+  horizontal: 0,
+  depth: 0,
+  aim: 0
+});
+
 console.log(`Part1: ${finalPositionOne.horizontal * finalPositionOne.depth}`);
+console.log(`Part2: ${finalPositionTwo.horizontal * finalPositionTwo.depth}`);
