97b2ae2784b09bee31ec1f885c72227e73db03bb
Sam Tyler <sam@sammtyler.com>
2021-12-13 21:07:50 +1030

    Solve day 9 part 2

--- day9/solution.js
+++ day9/solution.js
@@ -1,16 +1,17 @@
 const input = require('./input');
 
+const lowPointCoords = [];
 const lowPoints = [];
 
 function getAtCoords(row, column) {
   if (row < 0 || column < 0) {
-    return Infinity;
+    return 9;
   }
   if (row > input.length - 1) {
-    return Infinity;
+    return 9;
   }
   if (column > input[0].length - 1) {
-    return Infinity;
+    return 9;
   }
   return input[row][column];
 };
@@ -22,10 +23,37 @@ input.forEach((row, rowIndex) => {
     const right = getAtCoords(rowIndex, columnIndex + 1);
     const below = getAtCoords(rowIndex + 1, columnIndex);
     if (value < above && value < left && value < right && value < below) {
+      lowPointCoords.push([rowIndex, columnIndex]);
       lowPoints.push(value);
     }
   });
 });
 
-const answer = lowPoints.map(point => point + 1).reduce((a, b) => a + b);
-console.log(`Answer: ${answer}`);
+function getBasinSize(startingCoords) {
+  const searchStack = [startingCoords];
+  const pointsInBasin = [];
+  while (searchStack.length > 0) {
+    const [row, column] = searchStack.pop();
+    if (getAtCoords(row, column) === 9) {
+      continue;
+    }
+    if (pointsInBasin.includes(`${row},${column}`)) {
+      continue;
+    }
+    pointsInBasin.push(`${row},${column}`);
+    searchStack.push(
+      [row - 1, column],
+      [row, column - 1],
+      [row, column + 1],
+      [row + 1, column]
+    );
+  }
+  return pointsInBasin.length;
+}
+
+const answerOne = lowPoints.map(point => point + 1).reduce((a, b) => a + b);
+console.log(`Answer Part One: ${answerOne}`);
+
+// Get the three biggest basins
+const [a,b,c] = lowPointCoords.map(getBasinSize).sort((a, b) => a - b).reverse();
+console.log(`Answer Part Two: ${a * b * c}`);
