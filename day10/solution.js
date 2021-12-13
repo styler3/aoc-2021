4cb54c5102306d01922802da2872c00a45b36655
Sam Tyler <sam@sammtyler.com>
2021-12-14 07:46:28 +1030

    Solve day 10 part 2

--- day10/solution.js
+++ day10/solution.js
@@ -14,6 +14,13 @@ const illegalCharacterScores = {
   '>': 25137
 };
 
+const autocompleteScores = {
+  ')': 1,
+  ']': 2,
+  '}': 3,
+  '>': 4
+}
+
 function doMatch(open, close) {
   return bracketPairs[open] === close;
 }
@@ -33,9 +40,42 @@ function getFirstError(chunk) {
   return 0;
 }
 
+function isValid(chunk) {
+  return getFirstError(chunk) === 0;
+}
+
+function getFinishingCharacters(chunk) {
+  const openStack = [];
+  for (character of chunk) {
+    if (Object.keys(bracketPairs).includes(character)) {
+      openStack.push(character);
+      continue;
+    }
+    // We can assume they're valid here so we don't atually need to check the
+    // ending character at all
+    openStack.pop();
+  }
+  return openStack.map(character => bracketPairs[character]).reverse();
+}
+
+function getAutocompleteScore(characters) {
+  return characters.reduce((total, character) => {
+    return total * 5 + autocompleteScores[character];
+  }, 0)
+}
+
 function partOne() {
   const answer = input.map(getFirstError).reduce((a, b) => a + b);
   console.log(`Answer Part One: ${answer}`);
 };
 
+function partTwo() {
+  const validInput = input.filter(isValid);
+  const completions = validInput.map(getFinishingCharacters)
+  const autocompleteScores = completions.map(getAutocompleteScore);
+  const answer = autocompleteScores.sort((a, b) => a - b)[Math.floor(autocompleteScores.length / 2)];
+  console.log(`Answer Part Two: ${answer}`)
+}
+
 partOne();
+partTwo();
